0665919bfc03f12d0c1b67285e0f628b13f07d1a
quyen6 <quyenthai.060603@gmail.com>
2025-06-03 13:50:02 +0700

    set-up&design-header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Quinn_MQ</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/" className="nav-link">
+              Home{" "}
+            </NavLink>
+            <NavLink to="/users" className="nav-link">
+              Users{" "}
+            </NavLink>
+            <NavLink to="/admin" className="nav-link">
+              Admin{" "}
+            </NavLink>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Log in</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Log out</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
