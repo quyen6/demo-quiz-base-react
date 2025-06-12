14fed44ee6486e57cf52928f6fd8641cdc35ff12
quyen6 <quyenthai.060603@gmail.com>
2025-06-12 17:40:16 +0700

    Redux-persist-handle-data-when-f5-refresh

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import "nprogress/nprogress.css";
-
+import { PersistGate } from "redux-persist/integration/react";
 import Layout from "./Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <Layout />
-      </React.StrictMode>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <Layout />
+        </React.StrictMode>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
